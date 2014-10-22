684aa2cabcfde928867fe0fa80c5fb11da596499
hedgerh <hedgerh@gmail.com>
2014-10-22 04:45:31 -0400

    Added public states

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 angular.module('olivegardenApp', ['ngCookies', 'ui-router', 'ui.codemirror'])
-  .config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-      controller: 'ChallengeController',
-      templateUrl: '../app/views/dashboard.html'
-    });
-});
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+
+    $stateProvider
+        .state('public', {
+            abstract: true,
+            template: "<ui-view/>",
+            data: {
+                access: access.public
+            }
+        })
+        .state('public.404', {
+            url: '/404/',
+            templateUrl: '404'
+        });
+}]);
