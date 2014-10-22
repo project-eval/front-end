2fa153f898710bf15d893a73894afcdc42354728
hedgerh <hedgerh@gmail.com>
2014-10-22 04:48:49 -0400

    Added user and admin routes

--- app/app.js
+++ app/app.js
@@ -1,36 +1,66 @@
 angular.module('olivegardenApp', ['ngCookies', 'ui-router', 'ui.codemirror'])
-  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
-    $stateProvider
-        .state('public', {
-            abstract: true,
-            template: "<ui-view/>",
-            data: {
-                access: access.public
-            }
-        })
-        .state('public.404', {
-            url: '/404/',
-            templateUrl: '404'
-        });
+        // Public routes
+        $stateProvider
+            .state('public', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.public
+                }
+            })
+            .state('public.404', {
+                url: '/404/',
+                templateUrl: '404'
+            });
 
-    // Anonymous routes
-    $stateProvider
-        .state('anon', {
-            abstract: true,
-            template: "<ui-view/>",
-            data: {
-                access: access.anon
-            }
-        })
-        .state('anon.login', {
-            url: '/login/',
-            templateUrl: 'login',
-            controller: 'LoginCtrl'
-        })
-        .state('anon.register', {
-            url: '/register/',
-            templateUrl: 'register',
-            controller: 'RegisterCtrl'
-        });
-}]);
+        // Anonymous routes
+        $stateProvider
+            .state('anon', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.anon
+                }
+            })
+            .state('anon.login', {
+                url: '/login/',
+                templateUrl: 'login',
+                controller: 'LoginCtrl'
+            })
+            .state('anon.register', {
+                url: '/register/',
+                templateUrl: 'register',
+                controller: 'RegisterCtrl'
+            });
+
+        // Regular user routes
+        $stateProvider
+            .state('user', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.user
+                }
+            })
+            .state('user.home', {
+                url: '/',
+                templateUrl: 'home'
+            });
+
+        // Admin routes
+        $stateProvider
+            .state('admin', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.admin
+                }
+            })
+            .state('admin.admin', {
+                url: '/admin/',
+                templateUrl: 'admin',
+                controller: 'AdminCtrl'
+            });
+    }]);
