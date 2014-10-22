12082a105ab846b8d5cd165a9f8d1aa2c30204ad
hedgerh <hedgerh@gmail.com>
2014-10-22 05:09:59 -0400

    Added user service

---
+++ client/js/services/user.js
@@ -0,0 +1,8 @@
+angular.module('olivegardenApp')
+.factory('Users', function($http) {
+    return {
+        getAll: function(success, error) {
+            $http.get('/users').success(success).error(error);
+        }
+    };
+});
