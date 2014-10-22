c9615389b66de3c0bf6e2290d24743fa5f201133
hedgerh <hedgerh@gmail.com>
2014-10-22 13:36:30 -0400

    added Foxandxss' work flow. reorganized file structure

---
+++ app/js/app.js
@@ -0,0 +1,130 @@
+angular.module('olivegardenApp', ['ngCookies', 'ui.router', 'ui.codemirror'])
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+
+        // Public routes
+        $stateProvider
+            .state('public', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.public
+                }
+            })
+            .state('public.404', {
+                url: '/404/',
+                templateUrl: 'common/templates/404.tpl.html'
+            });
+
+        // Anonymous routes
+        $stateProvider
+            .state('anon', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.anon
+                }
+            })
+            .state('anon.login', {
+                url: '/login/',
+                templateUrl: 'apps/login/login.tpl.html',
+                controller: 'LoginCtrl'
+            });
+
+        // Regular user routes
+        $stateProvider
+            .state('user', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.user
+                }
+            })
+            .state('user.dashboard', {
+                url: '/',
+                templateUrl: 'apps/dashboard/dashboard.tpl.html'
+            })
+            .state('user.challenge', {
+                url: '/challenge',
+                templateUrl: 'apps/challenge/challenge.tpl.html'
+            });
+
+        // Admin routes
+        $stateProvider
+            .state('admin', {
+                abstract: true,
+                template: "<ui-view/>",
+                data: {
+                    access: access.admin
+                }
+            })
+            .state('admin.admin', {
+                url: '/admin/',
+                templateUrl: 'apps/admin/admin.tpl.html',
+                controller: 'AdminCtrl'
+            });
+
+        $urlRouterProvider.otherwise('/404');
+
+        // FIX for trailing slashes. Gracefully "borrowed" from https://github.com/angular-ui/ui-router/issues/50
+        $urlRouterProvider.rule(function($injector, $location) {
+            if ($location.protocol() === 'file')
+                return;
+
+            var path = $location.path(),
+                // Note: misnomer. This returns a query object, not a search string
+                search = $location.search(),
+                params;
+
+            // check to see if the path already ends in '/'
+            if (path[path.length - 1] === '/') {
+                return;
+            }
+
+            // If there was no search string / query params, return with a `/`
+            if (Object.keys(search).length === 0) {
+                return path + '/';
+            }
+
+            // Otherwise build the search string and return a `/?` prefix
+            params = [];
+            angular.forEach(search, function(v, k) {
+                params.push(k + '=' + v);
+            });
+            return path + '/?' + params.join('&');
+        });
+
+        $locationProvider.html5Mode(true);
+
+        $httpProvider.interceptors.push(function($q, $location) {
+            return {
+                'responseError': function(response) {
+                    if (response.status === 401 || response.status === 403) {
+                        $location.path('/login');
+                    }
+                    return $q.reject(response);
+                }
+            };
+        });
+    }])
+    .run(['$rootScope', '$state', 'Auth', function($rootScope, $state, Auth) {
+
+        $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams) {
+
+            if (!('data' in toState) || !('access' in toState.data)) {
+                $rootScope.error = "Access undefined for this state";
+                event.preventDefault();
+            } else if (!Auth.authorize(toState.data.access)) {
+                $rootScope.error = "Seems like you tried accessing a route you don't have access to...";
+                event.preventDefault();
+
+                if (fromState.url === '^') {
+                    if (Auth.isLoggedIn()) {
+                        $state.go('user.home');
+                    } else {
+                        $rootScope.error = null;
+                        $state.go('anon.login');
+                    }
+                }
+            }
+        });
+    }]);
--- client/js/routes.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-module.exports = function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      controller: require('./controllers/challenge.js'),
-      template: fs.readFileSync(__dirname + '/views/challenge.html')
-    });
-};
