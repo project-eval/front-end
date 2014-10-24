29244b177fd9b1e97974deb28eeea50d7b1c5fb3
olivierrr <oliviersinbox@gmail.com>
2014-10-24 15:52:35 -0700

    breadstick query

--- app/js/controllers/breadsticksCtrl.js
+++ app/js/controllers/breadsticksCtrl.js
@@ -5,10 +5,22 @@ var controllersModule = require('./_index');
 /**
  * 'breadstick' list
  * @controller
+ * @todo query filters
  */
 function BreadsticksCtrl(Challenges) {
+	var self = this;
 
+	this.data = [];
 
+	Challenges.query().then(onSuccess, onError);
+
+	function onSuccess (res) {
+		self.data = res.data;
+	}
+
+	function onError (err) {
+		throw err;
+	}
 
 };
 
--- app/js/routes.js
+++ app/js/routes.js
@@ -91,27 +91,16 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider, $httpProv
 
 	$urlRouterProvider.otherwise('/');
 
-	// FIX for trailing slashes. Gracefully "borrowed" from https://github.com/angular-ui/ui-router/issues/50
+	/*
+	 * FIX for trailing slashes. Gracefully "borrowed" from https://github.com/angular-ui/ui-router/issues/50
+	 */
 	$urlRouterProvider.rule(function($injector, $location) {
 		if ($location.protocol() === 'file') return;
-
-		var path = $location.path(),
-			// Note: misnomer. This returns a query object, not a search string
-			search = $location.search(),
-			params;
-
-		// check to see if the path already ends in '/'
+		var path = $location.path(), search = $location.search(), params;
 		if (path[path.length - 1] === '/') return;
-
-		// If there was no search string / query params, return with a `/`
 		if (Object.keys(search).length === 0) return path + '/';
-
-		// Otherwise build the search string and return a `/?` prefix
 		params = [];
-		angular.forEach(search, function(v, k) {
-			params.push(k + '=' + v);
-		});
-
+		angular.forEach(search, function(v, k) {params.push(k + '=' + v);});
 		return path + '/?' + params.join('&');
 	});
 
