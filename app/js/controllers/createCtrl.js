7fcbba66ddea9ecf8775e38471455272e715812d
olivierrr <oliviersinbox@gmail.com>
2014-10-25 15:12:35 -0700

    ui-view is fluid

--- app/js/controllers/createCtrl.js
+++ app/js/controllers/createCtrl.js
@@ -4,6 +4,8 @@ var controllersModule = require('./_index');
 
 /**
  * create a new 'breadstick'
+ * a breadstick is composed of several 'challenges' which may contain tests and a descriptio
+ *
  * @controller
  */
 function CreateCtrl(Challenges, AppSettings) {
@@ -22,6 +24,7 @@ function CreateCtrl(Challenges, AppSettings) {
 		challenges: [new Challenge(1), new Challenge(2)]
 	};
 
+	// send off to server
 	this.submit = function (data) {
 
 		Challenges.submit(data).then(onSuccess, onError);
@@ -39,11 +42,13 @@ function CreateCtrl(Challenges, AppSettings) {
 	// push a new challenge to data.challenges array
 	this.addChallenge = function () {
 		self.data.challenges.push(new Challenge(self.data.challenges.length+1));
+		self.radioModel = self.data.challenges.length-1;
 	}
 
-	function Challenge(index, description, test) {
-		this.description = description || '#write your description for challenge #' + index + ' here! \n take advantage of the markdown!';
-		this.test = test || 'write your tests for challenge #' + index + ' here!';	
+	function Challenge(index) {
+		var index = index || 0;
+		this.description = '#write your description for challenge #' + index + ' here! \n take advantage of the markdown!';
+		this.test = 'write your tests for challenge #' + index + ' here!';
 	}
 };
 
