6ba19d515d563d01d190ceaedda18e8afbfd4e22
olivierrr <oliviersinbox@gmail.com>
2014-10-25 03:45:00 -0700

    progress

--- app/js/controllers/createCtrl.js
+++ app/js/controllers/createCtrl.js
@@ -7,14 +7,18 @@ var controllersModule = require('./_index');
  * @controller
  */
 function CreateCtrl(Challenges, AppSettings) {
+	var self = this;
 
 	this.devMode = AppSettings.devMode;
 
+	// challenge index we are currently showing
+	this.radioModel = 0;
+
 	this.data = {
-		source: 'aa',
 		language: 'javascript',
 		difficulty: '42',
-		title: 'my breadstick'
+		title: 'my breadstick',
+		challenges: [new Challenge(1), new Challenge(2)]
 	};
 
 	this.submit = function (data) {
@@ -30,6 +34,15 @@ function CreateCtrl(Challenges, AppSettings) {
 		}
 
 	};
+
+	this.addChallenge = function () {
+		self.data.challenges.push(new Challenge(self.data.challenges.length+1));
+	}
+
+	function Challenge(index, description, test) {
+		this.description = description || '#write your description for challenge #' + index + ' here! \n take advantage of the markdown!';
+		this.test = test || 'write your tests for challenge #' + index + ' here!';	
+	}
 };
 
 controllersModule.controller('CreateCtrl', CreateCtrl);
