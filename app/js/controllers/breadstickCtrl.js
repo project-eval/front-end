1a18df2200d780db6a539b40fa1cb9d85013bd66
olivierrr <oliviersinbox@gmail.com>
2014-10-28 03:39:02 -0700

    updated breadstick style

--- app/js/controllers/breadstickCtrl.js
+++ app/js/controllers/breadstickCtrl.js
@@ -4,28 +4,49 @@ var controllersModule = require('./_index');
 
 /**
  * @controller
+ *
+ * onSubmit: test the code localy while the user works on an aswer
+ * onFinalSubmit: test the code remotely to make sure the user isn't cheating!
  */
 function BreadstickCtrl($stateParams, Challenges) {
 	var self = this;
 
+	// project information
+	// includes challenges descriptions and tests
 	self.data = {};
 
-	self.onTest = function () {
+	// which challenge within the project is the user training on?
+	self.challengeIndex = 0;
 
+	// code written by user
+	self.code = '';
+
+	// test report
+	self.output = '';
+
+	// local code eval
+	self.onSubmit = function () {
+		//
 	}
 
-	self.onSubmit = function () {
+	// remote code eval
+	self.onFinalSubmit = function () {
 		// Challenges.eval({
 		// 	id: breadstick_id,
 		// 	src: src
 		// });
 	}
 
+	function loadTest () {
+
+	}
+
 	function fetchChallenge () {
 		Challenges.getById($stateParams.id).then(onSuccess, onError);
 		function onSuccess (res) {
 			self.data = res.data.success;
-			console.log(res.data.success)
+			console.log(res.data.success);
+			loadTest()
 		}
 		function onError (err) {
 			throw err;
@@ -33,6 +54,7 @@ function BreadstickCtrl($stateParams, Challenges) {
 	}
 
 	fetchChallenge()
+
 };
 
 controllersModule.controller('BreadstickCtrl', BreadstickCtrl);
